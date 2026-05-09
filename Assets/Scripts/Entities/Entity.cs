4e7050fbc44cce86ee4c0ba2199776ffee55c07f
JupiterO5 <145288114+JupiterO5@users.noreply.github.com>
2026-05-08 19:27:41 -0600

    idamageable methods now virtual in entity

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -11,8 +11,8 @@ namespace STARGAZER.Entities
         public virtual DamageProfile DamageProfile { get; protected set; } = new DamageProfile();
         public virtual DefenseProfile DefenseProfile { get; protected set; } = new DefenseProfile();
 
-        public bool CanCrit(DamageInstance incomingAttack) => true;
-        public HitData TakeHit(DamageInstance[] incomingAttack)
+        public virtual bool CanCrit(DamageInstance incomingAttack) => true;
+        public virtual HitData TakeHit(DamageInstance[] incomingAttack)
         {
             bool critLands = incomingAttack[0].QualifiesCrit && CanCrit(incomingAttack[0]);
             var appliedAttack = critLands ? incomingAttack[1] : incomingAttack[0];
@@ -32,8 +32,8 @@ namespace STARGAZER.Entities
 
             return new HitData(this, damageReceived, critLands);
         }
-        public void OnReceiveCrit() { }
-        public void OnDeath() { }
+        public virtual void OnReceiveCrit() { }
+        public virtual void OnDeath() { }
     }
     public class HealthComponent
     {
--- Assets/Scripts/Entities/TestEntity.cs
+++ Assets/Scripts/Entities/TestEntity.cs
@@ -1,11 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
-using STARGAZER.Systems.Stats.StatProfiles;
-
-namespace STARGAZER.Entities
+﻿namespace STARGAZER.Entities
 {
-	public class TestEntity : Entity
-	{
-		//public override DefenseProfile DefenseProfile { get; protected set; } = new DefenseProfile();
-	}
+	public class TestEntity : Entity { }
 }
