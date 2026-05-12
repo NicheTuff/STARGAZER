6d9658614c79d38200af7c3c6e2bde1cf9573a48
JupiterO5 <145288114+JupiterO5@users.noreply.github.com>
2026-05-12 15:13:33 -0600

    No need for defenseprofile in idamageable

--- Assets/Scripts/Systems/Damage/IDamageable.cs
+++ Assets/Scripts/Systems/Damage/IDamageable.cs
@@ -1,10 +1,9 @@
-﻿using STARGAZER.Systems.Stats.StatProfiles;
+using STARGAZER.Systems.Stats.StatProfiles;
 
 namespace STARGAZER.Systems.Damage
 {
     public interface IDamageable
     {
-        public DefenseProfile DefenseProfile {  get; }
         public bool CanCrit(DamageInstance incomingAttack);
         public HitData TakeHit(DamageInstance[] incomingAttack);
         public void OnReceiveCrit();
